14d0993ddaada9655be7a68235fd95b36d33a133
Ryan Clarian <rclarian@gmail.com>
2024-05-17 15:10:28 +0800

    #07 What is a Component - create first component
    #07 What is a Component - create first component

---
+++ angular-ekart/src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    template: '<h3>e-Commerse by RFC</h3>'
+})
+
+export class HeaderComponent{
+
+}
