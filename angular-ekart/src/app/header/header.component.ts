be10dfb585852bf8b92a8a3843301323adc1951e
Ryan Clarian <rclarian@gmail.com>
2024-05-23 15:29:25 +0800

    #12 Create Component using Angular CLI
    #12 Create Component using Angular CLI

--- angular-ekart/src/app/header/header.component.ts
+++ angular-ekart/src/app/header/header.component.ts
@@ -1,8 +1,14 @@
 import { Component } from "@angular/core";
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @Component({
     selector: 'app-header',
     standalone: true,
+    imports: [
+        TopMenuComponent,
+        MainMenuComponent
+      ],
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 
