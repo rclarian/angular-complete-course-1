f51038f71a4575672e9331ffced68a45cd196489
Ryan Clarian <rclarian@gmail.com>
2024-05-24 14:39:32 +0800

    #15 String Interpolation - {{}}
    #15 String Interpolation - {{}}

--- angular-ekart/src/app/app.component.ts
+++ angular-ekart/src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { TopHeaderComponent } from './top-header/top-header.component'
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { ProductListComponent } from './product-list/product-list.component'
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { TopHeaderComponent } from './top-header/top-header.component'
     CommonModule, 
     RouterOutlet,
     HeaderComponent,
-    TopHeaderComponent
+    TopHeaderComponent,
+    ProductListComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
---
+++ angular-ekart/src/app/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  product = {
+    name: 'iPhone 15 Pro Max',
+    price: 2999,
+    color: 'Red',
+    discount: 8.5,
+    inStock: 5
+  }
+
+  getDiscountedPrice(){
+    return (this.product.price - (this.product.price * this.product.discount / 100)).toFixed(2);
+  }
+}
