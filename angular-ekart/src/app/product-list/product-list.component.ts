caaec2dc21a7a0b92e58b21e4c2f56c5c71101af
Ryan Clarian <rclarian@gmail.com>
2024-05-24 15:49:04 +0800

    #16 Property Binding - use [ ] or bind-
    #16 Property Binding - use [ ] or bind-

--- angular-ekart/src/app/product-list/product-list.component.ts
+++ angular-ekart/src/app/product-list/product-list.component.ts
@@ -8,12 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  name = 'John Doe';
   product = {
     name: 'iPhone 15 Pro Max',
     price: 2999,
     color: 'Red',
     discount: 8.5,
-    inStock: 5
+    inStock: 20,
+    pImage: '/assets/images/iphone.png'
   }
 
   getDiscountedPrice(){
