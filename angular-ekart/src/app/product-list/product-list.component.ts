76362b2a8706c68f428b98bf2b8155cad10c8895
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-05-24 21:47:43 +0800

    #17 Event Binding - click event
    #17 Event Binding - click event

--- angular-ekart/src/app/product-list/product-list.component.ts
+++ angular-ekart/src/app/product-list/product-list.component.ts
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  name = 'John Doe';
+  //name = 'John Doe';
+  addToCart: number = 0;
+
   product = {
     name: 'iPhone 15 Pro Max',
     price: 2999,
@@ -21,4 +23,24 @@ export class ProductListComponent {
   getDiscountedPrice(){
     return (this.product.price - (this.product.price * this.product.discount / 100)).toFixed(2);
   }
+
+  onNameChange(event: any){
+    //this.name = event.target.value;
+
+  }
+
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--;
+    }
+  }
+
+  incrementCartValue(){
+    if(this.addToCart < this.product.inStock){
+      this.addToCart++;
+    }
+    
+  }
+
+
 }
