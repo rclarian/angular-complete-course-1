b0e94469e1ad4f5dba9cf2412a4132a6533b9841
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-05-26 17:57:11 +0800

    #20 ngFor Directive - *ngFor="let item of [2,3,4,5,6,8]"
    #20 ngFor Directive - *ngFor="let item of [2,3,4,5,6,8]"

--- angular-ekart/src/app/product-list/product-list.component.ts
+++ angular-ekart/src/app/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from './search/search.component'
+import {NgFor, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'product-list',
   standalone: true,
-  imports: [SearchComponent],
+  imports: [NgFor, NgForOf, SearchComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
