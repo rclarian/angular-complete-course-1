dac60e2693436dbb61ce7e3bbdf872ebd0d5122a
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-05-26 18:16:06 +0800

    #20 ngFor Directive
    #20 ngFor Directive

--- angular-ekart/src/app/header/main-menu/main-menu.component.ts
+++ angular-ekart/src/app/header/main-menu/main-menu.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import {NgFor, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'main-menu',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgForOf],
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.css'
 })
 export class MainMenuComponent {
-
+  mainMenuItems:string[] = ['Home', 'Products', 'Sale', 'About', 'Contact'];
 }
--- angular-ekart/src/app/product-list/product-list.component.ts
+++ angular-ekart/src/app/product-list/product-list.component.ts
@@ -10,39 +10,5 @@ import {NgFor, NgForOf} from "@angular/common";
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  //name = 'John Doe';
-  addToCart: number = 0;
-
-  product = {
-    name: 'iPhone 15 Pro Max',
-    price: 2999,
-    color: 'Red',
-    discount: 8.5,
-    inStock: 20,
-    pImage: '/assets/images/iphone.png'
-  }
-
-  getDiscountedPrice(){
-    return (this.product.price - (this.product.price * this.product.discount / 100)).toFixed(2);
-  }
-
-  onNameChange(event: any){
-    //this.name = event.target.value;
-
-  }
-
-  decrementCartValue(){
-    if(this.addToCart > 0){
-      this.addToCart--;
-    }
-  }
-
-  incrementCartValue(){
-    if(this.addToCart < this.product.inStock){
-      this.addToCart++;
-    }
-    
-  }
-
-
+  listOfString: string[] = ['Mark', 'Steve', 'Merry', 'John', 'Sarah'];
 }
