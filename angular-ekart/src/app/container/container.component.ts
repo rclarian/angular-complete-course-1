bad6734c1bf625b751f64a5d88a6450646adff91
Ryan Clarian <rclarian@gmail.com>
2024-05-28 13:48:25 +0800

    #21 Rendering List of Complex Type - *ngFor="let prod of products
    #21 Rendering List of Complex Type - *ngFor="let prod of products

--- angular-ekart/src/app/container/container.component.ts
+++ angular-ekart/src/app/container/container.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from './search/search.component'
 import {NgFor, NgForOf} from "@angular/common";
+import { ProductListComponent } from './product-list/product-list.component'
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [NgFor, NgForOf, SearchComponent],
+  imports: [NgFor, NgForOf, SearchComponent, ProductListComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css'
 })
