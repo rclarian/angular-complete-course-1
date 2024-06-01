0f6317fffd94f343258b0492bef82bf9fff8b97e
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-06-01 21:51:28 +0800

    #35 ng-content in Angular
    #35 ng-content in Angular

--- angular-ekart/src/app/container/container.component.ts
+++ angular-ekart/src/app/container/container.component.ts
@@ -3,11 +3,12 @@ import { SearchComponent } from './search/search.component';
 import {NgFor, NgIf, NgForOf, CommonModule} from "@angular/common";
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { FeaturedBrandsComponent } from './featured-brands/featured-brands.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [NgIf, NgFor, NgForOf, SearchComponent, ProductListComponent, ProductDetailComponent],
+  imports: [NgIf, NgFor, NgForOf, SearchComponent, ProductListComponent, ProductDetailComponent, FeaturedBrandsComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.css'
 })
