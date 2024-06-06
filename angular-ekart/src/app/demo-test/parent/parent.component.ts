8f87ec46712e9fe683049f9f2cf09b4838815f30
Ryan Clarian <rclarian@gmail.com>
2024-06-06 12:57:18 +0800

    #43 ngAfterContentChecked  and ngAfterContentInit are compent hook only
    #43 ngAfterContentChecked  and ngAfterContentInit are compent hook only

--- angular-ekart/src/app/demo-test/parent/parent.component.ts
+++ angular-ekart/src/app/demo-test/parent/parent.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, Input, OnChanges, SimpleChanges, OnInit, DoCheck, AfterContentInit, ContentChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, Input, OnChanges, SimpleChanges, OnInit, DoCheck, AfterContentInit, ContentChild, AfterContentChecked } from '@angular/core';
 import {NgFor, NgForOf, CommonModule} from "@angular/common";
 import { ChildComponent } from './child/child.component';
 import { TestComponent } from '../test/test.component';
@@ -10,7 +10,7 @@ import { TestComponent } from '../test/test.component';
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
-export class ParentComponent implements OnChanges, OnInit, DoCheck, AfterContentInit{
+export class ParentComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked{
   @ViewChild('para') paraEl: ElementRef;
 
   showParaValue(){
@@ -24,7 +24,7 @@ export class ParentComponent implements OnChanges, OnInit, DoCheck, AfterContent
   @ContentChild('temp') paraContent: ElementRef;
 
   constructor(){
-    console.log('Parent component constructor called');
+    //console.log('Parent component constructor called');
     // console.log(this.title);
     // console.log(this.message);
   }
@@ -32,21 +32,26 @@ export class ParentComponent implements OnChanges, OnInit, DoCheck, AfterContent
   ngOnChanges(changes: SimpleChanges){
     //console.log(this.message);
     //console.log(changes);
-    console.log('ngOnChanges hook called');
+    //console.log('ngOnChanges hook called');
   }
 
   ngOnInit(){
-    console.log('ngOnInit hook called');
+    //console.log('ngOnInit hook called');
     //console.log(this.tempPara.nativeElement.innerHTML);
   }
 
   ngDoCheck(){
     console.log('ngDoCheck hook called');
-    console.log('In ngDoCheck',  this.paraContent);
+    //console.log('In ngDoCheck',  this.paraContent);
   }
 
   ngAfterContentInit(){
     console.log('ngAfterContentInit hook called');
-    console.log('In ngAfterContentInit',  this.paraContent.nativeElement);
+    //console.log('In ngAfterContentInit',  this.paraContent.nativeElement);
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked hook called');
+    console.log('In ngAfterContentChecked',  this.paraContent.nativeElement);
   }
 }
