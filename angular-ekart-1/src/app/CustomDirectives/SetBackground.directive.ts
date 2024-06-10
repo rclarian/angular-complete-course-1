a7e1b9e59c504b45802339c09d28b55d66789078
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-06-10 20:19:14 +0800

    #52 Property Binding in Directives
    #52 Property Binding in Directives

--- angular-ekart-1/src/app/CustomDirectives/SetBackground.directive.ts
+++ angular-ekart-1/src/app/CustomDirectives/SetBackground.directive.ts
@@ -7,10 +7,11 @@ import { Directive, ElementRef, Input, OnInit, Renderer2 } from "@angular/core";
 export class SetBackground implements OnInit{
     //private element: ElementRef;
     //private renderer: Renderer2;
-    @Input('setBackground') backColor: string = '#36454F';
-    @Input() textColor: string = 'white';
-    
+    // @Input('setBackground') backColor: string = '#36454F';
+    // @Input() textColor: string = 'white';
 
+    @Input('setBackground') changeTextAndBackColor: {backColor: string, textColor: string}
+    
     constructor(private element: ElementRef, private renderer: Renderer2){
         //this.element = element;
         //this.renderer = renderer;
@@ -19,8 +20,8 @@ export class SetBackground implements OnInit{
     ngOnInit(){
         // this.element.nativeElement.style.backgroundColor = '#36454F';
         // this.element.nativeElement.style.color = 'white';
-        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.backColor);
-        this.renderer.setStyle(this.element.nativeElement, 'color', this.textColor);
+        this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', this.changeTextAndBackColor.backColor);
+        this.renderer.setStyle(this.element.nativeElement, 'color', this.changeTextAndBackColor.textColor);
         //this.renderer.setAttribute(this.element.nativeElement, 'title', 'This is example title');
     }
 }
