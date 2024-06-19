41b2a1f3e0d2557100b54d1f956a90ac832e6d1e
Ryan Clarian <rclarian@gmail.com>
2024-06-19 14:48:36 +0800

    #69 Creating & Using an Observable
    #69 Creating & Using an Observable

--- angular-ekart-1/src/app/demo-test/demo-test.component.ts
+++ angular-ekart-1/src/app/demo-test/demo-test.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { SubscribeService } from '../Services/subscribe.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'demo-test',
@@ -67,4 +68,29 @@ export class DemoTestComponent {
     this.tab = 'user';
   }
 
+  //#69 Creating & Using an Observable
+  data: any[] = [];
+
+  //1.Create an OBSERVABLE
+
+  //Observable
+  myObservable = new Observable((observer) => {
+    //observer.next([1, 2, 3, 4, 5]); //array
+    setTimeout(() => { observer.next(1)}, 1000); //data streaming
+    setTimeout(() => { observer.next(2)}, 2000);
+    setTimeout(() => { observer.next(3)}, 3000);
+    setTimeout(() => { observer.next(4)}, 4000);
+    setTimeout(() => { observer.next(5)}, 5000);
+  });
+
+  GetAsyncData(){
+
+    //Observer
+    //call back function next, error, complete
+    this.myObservable.subscribe((val: any) => {
+      //this.data = val; //for array
+      this.data.push(val);
+    });
+  }
+  //end #69
 }
