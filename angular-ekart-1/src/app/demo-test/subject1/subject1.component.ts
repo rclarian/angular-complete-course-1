8da35a3a4af600f065cbebfbe6fbfe0697116a0b
Ryan Clarian <rclarian@gmail.com>
2024-06-20 20:41:21 +0800

    #75 Observable vs Subjects - observable call api
    #75 Observable vs Subjects - observable call api

--- angular-ekart-1/src/app/demo-test/subject1/subject1.component.ts
+++ angular-ekart-1/src/app/demo-test/subject1/subject1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-subject1',
@@ -10,16 +11,22 @@ export class Subject1Component implements OnInit{
 
   ngOnInit(){
     //let obs = new Observable((observer) => {observer.next(Math.random())});
-    const subject = new Subject();
+    //const subject = new Subject();
 
     //Subscriber 1
-    subject.subscribe((data) => {console.log(data)});
+    //subject.subscribe((data) => {console.log(data)});
 
     //Subscriber 2
-    subject.subscribe((data) => {console.log(data)});
+    //subject.subscribe((data) => {console.log(data)});
 
-    subject.next(Math.random());
+    //subject.next(Math.random());
 
+    //Ajax call
+    const data = ajax('https://randomuser.me/api/');
+
+    data.subscribe((res) => {console.log(res)});
+    data.subscribe((res) => {console.log(res)});
+    data.subscribe((res) => {console.log(res)});
   }
 
 }
