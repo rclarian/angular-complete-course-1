a83da4933bd95e6c71521995e411590016517d1c
Ryan Clarian <rclarian@gmail.com>
2024-06-20 19:39:09 +0800

    #74 Subjects in RxJS - service using Subjects
    #74 Subjects in RxJS - service using Subjects

--- angular-ekart-1/src/app/Services/task.service.ts
+++ angular-ekart-1/src/app/Services/task.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  CreateTask: EventEmitter<string> = new EventEmitter<string>();
+  //CreateTask: EventEmitter<string> = new EventEmitter<string>();
+  CreateTask = new Subject<string>();
 
   OnCreateTask(value: string){
-    this.CreateTask.emit(value);
+    //this.CreateTask.emit(value);
+    this.CreateTask.next(value);
   }
 }
