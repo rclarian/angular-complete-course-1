17e6523da969bb82ea1c05815c8be78da7514d1e
Ryan Clarian <rclarian@gmail.com>
2024-06-21 08:19:46 +0800

    #76 Behavior Subject - we can use initial value on BehaviorSubject
    #76 Behavior Subject - we can use initial value on BehaviorSubject

--- angular-ekart-1/src/app/demo-test/subject1/subject1.component.ts
+++ angular-ekart-1/src/app/demo-test/subject1/subject1.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -12,24 +12,28 @@ export class Subject1Component implements OnInit{
   ngOnInit(){
     //let obs = new Observable((observer) => {observer.next(Math.random())});
     //const subject = new Subject();
+    const subject = new BehaviorSubject<number>(1000);
 
     //Subscriber 1
-    //subject.subscribe((data) => {console.log(data)});
+    subject.subscribe((data) => {console.log('result 1' + ' = ' + data)});
 
     //Subscriber 2
-    //subject.subscribe((data) => {console.log(data)});
+    subject.subscribe((data) => {console.log('result 2' + ' = ' + data)});
 
-    //subject.next(Math.random());
+    subject.next(Math.random());
+
+    //Subscriber 3
+    subject.subscribe((data) => {console.log('result 3' + ' = ' + data)});
 
     //Ajax call
-    const subject = new Subject();
-    const data = ajax('https://randomuser.me/api/');
+    // const subject = new Subject();
+    // const data = ajax('https://randomuser.me/api/');
 
-    subject.subscribe((res) => {console.log(res)});
-    subject.subscribe((res) => {console.log(res)});
-    subject.subscribe((res) => {console.log(res)});
+    // subject.subscribe((res) => {console.log(res)});
+    // subject.subscribe((res) => {console.log(res)});
+    // subject.subscribe((res) => {console.log(res)});
 
-    data.subscribe(subject);
+    // data.subscribe(subject);
   }
 
 }
