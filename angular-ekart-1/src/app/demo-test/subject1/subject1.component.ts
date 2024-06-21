a006292a2ce26476c9a16a3e900249f8c01d9f1b
Ryan Clarian <rclarian@gmail.com>
2024-06-21 09:02:01 +0800

    #78 Async Subject - return the last emitted value after the complete method call
    #78 Async Subject - return the last emitted value after the complete method call

--- angular-ekart-1/src/app/demo-test/subject1/subject1.component.ts
+++ angular-ekart-1/src/app/demo-test/subject1/subject1.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -13,25 +13,25 @@ export class Subject1Component implements OnInit{
     //let obs = new Observable((observer) => {observer.next(Math.random())});
     //const subject = new Subject();
     //const subject = new BehaviorSubject<number>(1000);
-    const subject = new ReplaySubject(2);
+    // const subject = new ReplaySubject(2);
 
-    subject.next(100);
-    subject.next(200);
-    subject.next(300);
+    // subject.next(100);
+    // subject.next(200);
+    // subject.next(300);
 
     //Subscriber 1
-    subject.subscribe((data) => {console.log('Subscriber 1' + ' = ' + data)});
+    //subject.subscribe((data) => {console.log('Subscriber 1' + ' = ' + data)});
 
     //Subscriber 2
-    subject.subscribe((data) => {console.log('Subscriber 2' + ' = ' + data)});
+    //subject.subscribe((data) => {console.log('Subscriber 2' + ' = ' + data)});
 
     //subject.next(Math.random());
-    subject.next(2020);
+    //subject.next(2020);
 
     //Subscriber 3
-    subject.subscribe((data) => {console.log('Subscriber 3' + ' = ' + data)});
+    //subject.subscribe((data) => {console.log('Subscriber 3' + ' = ' + data)});
 
-    subject.next(2023);
+    //subject.next(2023);
 
     //Ajax call
     // const subject = new Subject();
@@ -42,6 +42,19 @@ export class Subject1Component implements OnInit{
     // subject.subscribe((res) => {console.log(res)});
 
     // data.subscribe(subject);
+
+    //Async Subject
+    const asyncSubject = new AsyncSubject();
+
+    asyncSubject.next(100);
+    asyncSubject.next(200);
+    asyncSubject.next(300);
+    asyncSubject.complete();
+    asyncSubject.next(400);
+
+    asyncSubject.subscribe((data) => {console.log('Subscriber 1' + ' = ' + data)});
+    asyncSubject.subscribe((data) => {console.log('Subscriber 2' + ' = ' + data)});
+
   }
 
 }
