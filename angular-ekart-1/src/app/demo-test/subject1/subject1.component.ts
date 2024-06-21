3326d8fa7aac4545140edc4019fad22bc8225534
Ryan Clarian <rclarian@gmail.com>
2024-06-21 08:45:07 +0800

    #77 Replay Subject - replays old values to new subscribers
    #77 Replay Subject - replays old values to new subscribers

--- angular-ekart-1/src/app/demo-test/subject1/subject1.component.ts
+++ angular-ekart-1/src/app/demo-test/subject1/subject1.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, ReplaySubject, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -12,18 +12,26 @@ export class Subject1Component implements OnInit{
   ngOnInit(){
     //let obs = new Observable((observer) => {observer.next(Math.random())});
     //const subject = new Subject();
-    const subject = new BehaviorSubject<number>(1000);
+    //const subject = new BehaviorSubject<number>(1000);
+    const subject = new ReplaySubject(2);
+
+    subject.next(100);
+    subject.next(200);
+    subject.next(300);
 
     //Subscriber 1
-    subject.subscribe((data) => {console.log('result 1' + ' = ' + data)});
+    subject.subscribe((data) => {console.log('Subscriber 1' + ' = ' + data)});
 
     //Subscriber 2
-    subject.subscribe((data) => {console.log('result 2' + ' = ' + data)});
+    subject.subscribe((data) => {console.log('Subscriber 2' + ' = ' + data)});
 
-    subject.next(Math.random());
+    //subject.next(Math.random());
+    subject.next(2020);
 
     //Subscriber 3
-    subject.subscribe((data) => {console.log('result 3' + ' = ' + data)});
+    subject.subscribe((data) => {console.log('Subscriber 3' + ' = ' + data)});
+
+    subject.next(2023);
 
     //Ajax call
     // const subject = new Subject();
