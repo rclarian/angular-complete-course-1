f9edacd391cbba3e521b98ee00709bea90774391
Ryan Clarian <rclarian@gmail.com>
2024-06-24 16:12:29 +0800

    #88 Retrieving Route Parameter Value - this.activeRoute.params.subscribe,   this.activeRoute.paramMap.subscribe
    #88 Retrieving Route Parameter Value - this.activeRoute.params.subscribe,   this.activeRoute.paramMap.subscribe

--- angular-router/src/app/courses/course-detail/course-detail.component.ts
+++ angular-router/src/app/courses/course-detail/course-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { Course } from '../../Models/course';
 import { CourseService } from '../../Services/course.service';
 import { ActivatedRoute } from '@angular/router';
@@ -8,16 +8,32 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './course-detail.component.html',
   styleUrl: './course-detail.component.css'
 })
-export class CourseDetailComponent implements OnInit{
+export class CourseDetailComponent implements OnInit, OnDestroy{
   selectedCourse: Course;
   courseId: number;
 
   courseService: CourseService = inject(CourseService);
   activeRoute: ActivatedRoute = inject(ActivatedRoute);
+  paramMapObs;
 
   ngOnInit(){
     //this.courseId = this.activeRoute.snapshot.params['id']; // old format
-    this.courseId = +this.activeRoute.snapshot.paramMap.get('id'); // plus convert string to number
-    this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId);
+    //this.courseId = +this.activeRoute.snapshot.paramMap.get('id'); // plus convert string to number
+
+    //params ----
+    // this.activeRoute.params.subscribe((data) => {
+    //   this.courseId = +data['id'];
+    //   this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId);
+    // })
+
+    //paramMap -----
+    this.paramMapObs = this.activeRoute.paramMap.subscribe((data) => {
+      this.courseId = +data.get('id');
+      this.selectedCourse = this.courseService.courses.find(course => course.id === this.courseId);
+    })
+  }
+
+  ngOnDestroy(){
+    this.paramMapObs.unsubscribe();
   }
 }
