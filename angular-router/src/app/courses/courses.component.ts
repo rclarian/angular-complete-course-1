4693ad6b2b42da0d1b7f55beece285e1744414ab
Ryan Clarian <rclarian@gmail.com>
2024-06-24 18:14:48 +0800

    #89 Using Query String in Route - this.activeRoute.queryParamMap.subscribe
    #89 Using Query String in Route - this.activeRoute.queryParamMap.subscribe

--- angular-router/src/app/courses/courses.component.ts
+++ angular-router/src/app/courses/courses.component.ts
@@ -1,13 +1,52 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CourseService } from '../Services/course.service';
 import { Course } from '../Models/course';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
   coursesService = inject(CourseService);
-  AllCourses: Course[] = this.coursesService.courses;
+  AllCourses: Course[];
+  searchString: string;
+
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(){
+    //this.searchString = this.activeRoute.snapshot.queryParams['search']; //queryString
+    //this.searchString = this.activeRoute.snapshot.queryParamMap.get('search'); //queryString
+    //console.log(this.searchString);
+
+    //queryParams
+    // this.activeRoute.queryParams.subscribe((data) => {
+    //   this.searchString = data['search'];
+
+    //   if(this.searchString === undefined || this.searchString === '' || this.searchString === null){
+    //     this.AllCourses = this.coursesService.courses; 
+    //   }else{
+    //     this.AllCourses = this.coursesService.courses
+    //       .filter(x => x.title.toLowerCase()
+    //       .includes(this.searchString.toLowerCase())); 
+    //   }
+
+    // })
+
+    //queryParamMap -- this is recommended to use
+    this.activeRoute.queryParamMap.subscribe((data) => {
+      this.searchString = data.get('search');
+
+      if(this.searchString === undefined || this.searchString === '' || this.searchString === null){
+        this.AllCourses = this.coursesService.courses; 
+      }else{
+        this.AllCourses = this.coursesService.courses
+          .filter(x => x.title.toLowerCase()
+          .includes(this.searchString.toLowerCase())); 
+      }
+
+    })
+
+  }
 }
--- angular-router/src/app/home/banner/banner.component.ts
+++ angular-router/src/app/home/banner/banner.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class BannerComponent {
 
+  router: Router = inject(Router);
+
+  OnSearchClicked(value: string){
+    //console.log(value);
+    this.router.navigate(['/Courses'], { queryParams: { search:  value }});
+  }
 }
