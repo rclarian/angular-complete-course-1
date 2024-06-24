453403e64d89f2ea0f4682141d34ca8d1e93ded9
Ryan Clarian <rclarian@gmail.com>
2024-06-24 14:46:44 +0800

    #86 Navigating between Routes Programmatically - navigate, navigateByUrl
    #86 Navigating between Routes Programmatically - navigate, navigateByUrl

--- angular-router/src/app/home/popular/popular.component.ts
+++ angular-router/src/app/home/popular/popular.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CourseService } from '../../Services/course.service';
 import { Course } from '../../Models/course';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-popular',
@@ -11,7 +12,16 @@ export class PopularComponent implements OnInit{
   courseService = inject(CourseService);
   popularCourses: Course[] = [];
 
+  router: Router = inject(Router);
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
   ngOnInit(){
     this.popularCourses = this.courseService.courses.filter(c => c.rating >= 4.5);
+  }
+
+  navigateToCourses(){
+    //this.router.navigate(['Courses'], {relativeTo: this.activeRoute}); //appended to active url
+    //this.router.navigate(['Courses']);//first method
+    this.router.navigateByUrl('Courses'); //second method
   }
 }
