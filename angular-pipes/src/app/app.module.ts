96f3a899d252e4c85aeedc815896f4af5345c90f
Ryan Clarian <rclarian@gmail.com>
2024-06-25 19:05:06 +0800

    #103 How to Create Custom Pipes
    #103 How to Create Custom Pipes

--- angular-pipes/src/app/app.module.ts
+++ angular-pipes/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { PercentagePipe } from './Pipes/percentage.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    PercentagePipe
   ],
   imports: [
     BrowserModule,
