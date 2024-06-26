17e64c30382b760c5adbdb2de47013e774e6eff0
Ryan Clarian <rclarian@gmail.com>
2024-06-26 22:06:46 +0800

    #120 setvalue and patchValue method - patchValue is better
    #120 setvalue and patchValue method - patchValue is better

--- template-driven-form/src/app/app.component.ts
+++ template-driven-form/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   firstName: string = '';
   lastName: string = '';
+  userDob: string = '';
   userEmail: string = '';
 
   @ViewChild('registrationForm') form: NgForm;
@@ -32,4 +33,55 @@ export class AppComponent {
     console.log(this.form.value.address.country);
     console.log(this.form.value.address.city);
   }
+
+  GenerateUsername(){
+    let username = '';
+
+    if(this.firstName.length >= 3){
+      username += this.firstName.slice(0, 3);
+    }else{
+      username += this.firstName;
+    }
+
+    if(this.lastName.length >= 3){
+      username += this.lastName.slice(0, 3);
+    }else{
+      username += this.lastName;
+    }
+
+    let datetime = new Date(this.userDob);
+    username += datetime.getFullYear();
+    username = username.toLowerCase();
+
+    console.log(username)
+
+    //this.form.value.username = username;
+    //this.form.controls['username'].value = username;
+
+    // this.form.setValue({
+    //   firstname: this.form.value.firstname,
+    //   lastname: this.form.value.lastname,
+    //   email: this.form.value.email,
+    //   dob: this.form.value.dob,
+    //   phone: this.form.value.phone,
+    //   gender: this.form.value.gender,
+    //   username: username,
+    //   address: {
+    //     street1: this.form.value.address.street1,
+    //     street2: this.form.value.address.street2,
+    //     country: this.form.value.address.country,
+    //     city: this.form.value.address.city,
+    //     region: this.form.value.address.region,
+    //     postal: this.form.value.address.postal
+    //   }
+    // });
+
+    this.form.form.patchValue({
+      username: username
+    });
+
+  }
+
+
+
 }
