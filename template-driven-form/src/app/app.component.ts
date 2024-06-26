ec667bf81046a84e90abdb26a29907ae3395bc94
Ryan Clarian <rclarian@gmail.com>
2024-06-26 18:15:37 +0800

    #116 Working with Radio Buttons - loop using ngFor
    #116 Working with Radio Buttons - loop using ngFor

--- template-driven-form/src/app/app.component.ts
+++ template-driven-form/src/app/app.component.ts
@@ -11,6 +11,12 @@ export class AppComponent {
 
   @ViewChild('registrationForm') form: NgForm;
 
+  genders = [
+    {id: 'check-male', value: 'male', display: 'Male'},
+    {id: 'check-female', value: 'female', display: 'Female'},
+    {id: 'check-other', value: 'other', display: 'Prefer not to say'}
+  ]
+
   OnFormSubmitted(){
     console.log(this.form);
     console.log(this.form.controls['firstname'].value);
