8efcdbf613a6c84ffe49d34b1e1c761fa0c0be00
Ryan Clarian <rclarian@gmail.com>
2024-06-28 10:33:30 +0800

    #137 Collecting Form Data to Insert | HTTP Client - passing of data from child to parent
    #137 Collecting Form Data to Insert | HTTP Client - passing of data from child to parent

--- angular-http-client/src/app/dashboard/create-task/create-task.component.ts
+++ angular-http-client/src/app/dashboard/create-task/create-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Task } from '../../Model/Task';
 
 @Component({
   selector: 'app-create-task',
@@ -10,16 +11,18 @@ export class CreateTaskComponent {
   @Output()
   CloseForm: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  //1. pass data - EventEmitter
   @Output()
-  EmitTaskData: EventEmitter<any> = new EventEmitter<any>();
+  EmitTaskData: EventEmitter<Task> = new EventEmitter<Task>();
 
   OnCloseForm(){
     this.CloseForm.emit(false);
   }
 
+  //2. pass data - emit on OnFormSubmitted()
   OnFormSubmitted(form: NgForm){
     this.EmitTaskData.emit(form.value);
-    console.log(form.value);
+    this.CloseForm.emit(false);
   }
 
 
--- angular-http-client/src/app/dashboard/dashboard.component.ts
+++ angular-http-client/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from '../Model/Task';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,4 +16,11 @@ export class DashboardComponent {
   CloseCreateTaskForm(){
     this.showCreateTaskForm = false;
   }
+
+  //4. pass data - get the data here CreateTask(data: Task)
+  CreateTask(data: Task){
+    console.log('Dashboard: ' + data);
+  }
+
+
 }
