f7d5e96cfb36cc340ad14050eafe9b9b8aea6d69
Ryan Clarian <rclarian@gmail.com>
2024-06-30 20:47:35 +0800

    #157 Creating Signup & Login Page - login code
    #157 Creating Signup & Login Page - login code

--- angular-authentication/src/app/login/login.component.ts
+++ angular-authentication/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  isLoginMode: boolean = true;
+
+  onSwitchMode(){
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onFormSubmitted(form: NgForm){
+    console.log(form.value);
+    form.reset();
+  }
 
 }
