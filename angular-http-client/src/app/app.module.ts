9ed1d7633deb1caa11813c364c675c73263cded6
Ryan Clarian <>
2024-06-30 10:02:48 +0800

    #153 Interceptors in Angular - create interceptor
    #153 Interceptors in Angular - create interceptor

--- angular-http-client/src/app/app.module.ts
+++ angular-http-client/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { HeaderComponent } from './header/header.component';
 import { CreateTaskComponent } from './dashboard/create-task/create-task.component';
 import { TaskDetailsComponent } from './dashboard/task-details/task-details.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptorService } from './Services/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    //provideClientHydration(),
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
   ],
   bootstrap: [AppComponent]
 })
