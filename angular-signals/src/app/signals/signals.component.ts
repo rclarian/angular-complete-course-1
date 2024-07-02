15dda713828d4e6811a91fba28f0a482881e4387
Ryan Clarian <rclarian@gmail.com>
2024-07-02 20:04:56 +0800

    #186 Mutating a Signal Value
    #186 Mutating a Signal Value

--- angular-signals/src/app/signals/signals.component.ts
+++ angular-signals/src/app/signals/signals.component.ts
@@ -8,17 +8,20 @@ import { Component, DoCheck, signal } from '@angular/core';
 export class SignalsComponent implements DoCheck{
   counter =  signal(0);
 
-  message: string[] = [];
+  message = signal<string[]>([]);
 
   increment(){
    //this.counter.set(this.counter() + 1);
-   this.counter.update((old) => old + 1)
-   this.message.push('Current counter value is: ' + this.counter);
+   this.counter.update((old) => old + 1);
+   //this.message.push('Current counter value is: ' + this.counter);
+   this.message.update((prevValue) => [...prevValue, 'Current value of counter is: ' + this.counter()]);
+   //this.message.mutate((prevValue) => prevValue.push('Current value of counter is: ' + this.counter()));
+   
   }
 
   decrement(){
-   this.counter;
-   this.message.pop();
+    this.counter.update((old) => old - 1);
+   //this.message.pop();
   }
 
   ngDoCheck(){
